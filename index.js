68475f960a005d3944f3ee7ee202589d67fc11ae
DaHarris <dharrisdnv@gmail.com>
2017-03-22 14:00:45 -0600

    add simple http request to backend

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 var express = require('express')
 var app = express()
+var bodyParser = require('body-parser')
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/hello', function (req, res) {
+  res.status(200).json({text: 'Hello World!'})
 })
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+  next()
+})
+
+app.use(bodyParser.json())
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
